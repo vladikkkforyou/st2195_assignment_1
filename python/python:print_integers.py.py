8cc759423344f7c5c5db3552bd3b0ca7c6d63054
Влада Рябоволова <vladik@Laptop-2.local>
2026-07-22 22:06:57 +0100

    Add Spyder project and Python integer printing script

---
+++ python/python:print_integers.py.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 22 21:58:02 2026
+
+@author: vladik
+"""
+import os
+
+os.chdir("/Users/vladik/st2195_assignment_1/python")
+
+print(os.getcwd())
+
+for number in range(1, 11):
+    print(number)
